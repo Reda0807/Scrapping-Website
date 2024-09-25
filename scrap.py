cd69ea48b2444d5684a7a98dad7bb09d6199b137
Reda0807 <hatorireda@gmail.com>
2024-09-26 03:08:17 +1000

    feat-:add scrapping code of the hotel_list

--- scrap.py
+++ scrap.py
@@ -1,11 +1,50 @@
 import requests
 import re
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import bs4
+import time
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36'
-}
+base_url = "https://www.booking.com/searchresults.en-gb.html?ss=italy"
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36'}
+driver = webdriver.Chrome()
+driver.get(base_url)
+page = driver.page_source
 
-rep = requests.get("https://www.booking.com/searchresults.en-gb.html?ss=Italy&ssne=Italy&ssne_untouched=Italy&efdco=1&label=gen173nr-1BCAEoggI46AdIM1gEaMIBiAEBmAEJuAEXyAEM2AEB6AEBiAIBqAIDuALHoNC3BsACAdICJDk4ZDFjNTM2LWRkNDMtNGU1ZC1hZWU1LTg0M2QxODkwZjMwZNgCBeACAQ&sid=ead78d14fd9e4d904d89b3da04ea788d&aid=304142&lang=en-gb&sb=1&src_elem=sb&src=searchresults&dest_id=104&dest_type=country&group_adults=2&no_rooms=1&group_children=0", headers=headers)
-html = rep.text
+def get_base_page():
+    scroll_to_bottom()
+    
+    return page
 
-print(html)
+def get_hotel_urls(page):
+    soup = bs4.BeautifulSoup(page, "html.parser")
+    url_els = soup.find_all("a", class_="a78ca197d0")
+    urls = []
+    for i in url_els:
+        temp = i["href"]
+        urls.append(temp[:temp.find('label')-1])
+    return urls
+
+def scroll_to_bottom():
+    while True:
+        if "Accept" in page:
+            button = driver.find_element(By.ID, "onetrust-accept-btn-handler")
+            button.click()
+
+        driver.execute_script("window.scrollTo(0, 500);")
+        time.sleep(5)
+
+        print("Load more results" in page)
+        if "Load more results" in page:
+            break
+
+def get_hotel_content(url):
+    pass
+
+if __name__ == "__main__":
+    base_page = get_base_page()
+    print(len(get_hotel_urls(base_page)))
+
+  
+    # rep = requests.get("https://www.booking.com/hotel/it/coccole-in-villa-b-amp-b-close-to-outlet.en-gb.html?aid=304142", headers=headers)
+    # print(rep.text)
